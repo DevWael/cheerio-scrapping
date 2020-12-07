4d088796f7c50dc4092c327fff99ce940ec073ff
devwael <dev.ahmedwael@gmail.com>
2020-12-07 15:28:30 +0200

    make request delays 5 seconds

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
+const util = require('util');
+const setTimeoutPromise = util.promisify(setTimeout);
 const { writeFileSync } = require('fs');
 
 const data = [];
 
 (async () => {
     for (var i = 1; i <= 43; i++) {
-        const url = 'https://www.amazon.sa/-/en/Mobile-Phones/b/?ie=UTF8&node=16966419031&page=' + i;
-        const { data: html } = await axios.get(url);
+        const url = 'https://www.amazon.sa/-/ar/Mobile-Phones/b/?ie=UTF8&node=16966419031&page=' + i;
+        
 
-        const $ = cheerio.load(html);
-        $('#mainResults .s-result-list li').each(function (index, element) {
-            data.push(
-                $(element).find('.a-link-normal').attr('href')
-            );
+        // const $ = cheerio.load(html);
+        // $('#mainResults .s-result-list li').each(function (index, element) {
+        //     data.push(
+        //         $(element).find('.a-link-normal').attr('href')
+        //     );
+        // });
+
+        await setTimeoutPromise(5000).then(async (value) => {
+            const { data: html } = await axios.get(url);
+            const $ = cheerio.load(html);
+            $('#mainResults .s-result-list li').each(function (index, element) {
+                data.push(
+                    $(element).find('.a-link-normal').attr('href')
+                );
+            });
+            console.log('COMPLETED', i);
         });
 
-        console.log('COMPLETED', i);
+        
     }
 
-    writeFileSync('./wael.json', JSON.stringify(data));
+    writeFileSync('./mobiles_ss.json', JSON.stringify(data));
 })();
